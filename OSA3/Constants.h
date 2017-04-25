48fa1d8deeddb11ac3de4459e63fe473797eb74b
Hassaan Fayyaz <hassaan.fayyaz@westagilelabs.com>
2017-04-25 14:58:48 +0500

    40 or so files are being created.

---
+++ OSA3/Constants.h
@@ -0,0 +1,16 @@
+//
+//  Constants.h
+//  OSA3
+//
+//  Created by Hassaan on 25/04/2017.
+//  Copyright © 2017 HaigaTech. All rights reserved.
+//
+
+#ifndef Constants_h
+#define Constants_h
+
+const int MAX_FILE_SIZE     = 1024;
+const int MAX_FILE_COUNT    = 40;
+
+
+#endif /* Constants_h */
--- OSA3/main.c
+++ OSA3/main.c
@@ -15,31 +15,52 @@
 #include <string.h>
 #include <errno.h>
 #include <fcntl.h>
+#include "helpers.h"
+#include "Constants.h"
 
-static const char *hello_str = "Hello World!\n";
-static const char *hello_path = "/hello";
+static const char *haiga_str = "Hello World!\n";
+static const char *haiga_path = "/hello";
+static char fileNamesArr[MAX_FILE_COUNT][MAX_FILE_SIZE];
 
-static int hello_getattr(const char *path, struct stat *stbuf)
+
+static int haiga_getattr(const char *path, struct stat *stbuf)
 {
+    printf("Get Attr FUNCTION Moun\n");
 	int res = 0;
 
 	memset(stbuf, 0, sizeof(struct stat));
 	if (strcmp(path, "/") == 0) {
 		stbuf->st_mode = S_IFDIR | 0755;
 		stbuf->st_nlink = 2;
-	} else if (strcmp(path, hello_path) == 0) {
-		stbuf->st_mode = S_IFREG | 0444;
-		stbuf->st_nlink = 1;
-		stbuf->st_size = strlen(hello_str);
-	} else
-		res = -ENOENT;
+        return res;
+	}
+    else if (strcmp(path, haiga_path) == 0) {
+        stbuf->st_mode = S_IFREG | 0777;
+        stbuf->st_nlink = 1;
+        stbuf->st_size = strlen(haiga_str);
+        return res;
+    }
+    else {
+        for (int i=0; i<40 ; i++) {
+            if (strcmp(path, fileNamesArr[i]) == 0) {
+                stbuf->st_mode = S_IFREG | 0777;
+                stbuf->st_nlink = 1;
+                stbuf->st_size = strlen(haiga_str);
+                return res;
+            }
+        }
+    }
+    
+    res = -ENOENT;
+    return res;
 
-	return res;
 }
 
-static int hello_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
+// Return one or more directory entries (struct dirent) to the caller. This is one of the most complex FUSE functions. It is related to, but not identical to, the readdir(2) and getdents(2) system calls, and the readdir(3) library function. Because of its complexity, it is described separately below. Required for essentially any filesystem, since it's what makes ls and a whole bunch of other things work.
+static int haiga_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
 			 off_t offset, struct fuse_file_info *fi)
 {
+    printf("READDIR FUNCTION \n");
 	(void) offset;
 	(void) fi;
 
@@ -48,49 +69,89 @@ static int hello_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
 
 	filler(buf, ".", NULL, 0);
 	filler(buf, "..", NULL, 0);
-	filler(buf, hello_path + 1, NULL, 0);
-
+	filler(buf, haiga_path + 1, NULL, 0);
+    for(int i=0 ; i<40 ; i++) {
+        filler(buf, fileNamesArr[i] + 1, NULL, 0);
+    }
 	return 0;
 }
 
-static int hello_open(const char *path, struct fuse_file_info *fi)
+
+static int haiga_open(const char *path, struct fuse_file_info *fi)
 {
-	if (strcmp(path, hello_path) != 0)
-		return -ENOENT;
-
-	if ((fi->flags & 3) != O_RDONLY)
-		return -EACCES;
+    printf("OPEN FUNCTION \n");
+//	if (strcmp(path, haiga_path) != 0)
+//		return -ENOENT;
+//
+//	if ((fi->flags & 3) != O_RDONLY)
+//		return -EACCES;
 
 	return 0;
 }
 
-static int hello_read(const char *path, char *buf, size_t size, off_t offset,
+// Read size bytes from the given file into the buffer buf, beginning offset bytes into the file. See read(2) for full details. Returns the number of bytes transferred, or 0 if offset was at or beyond the end of the file. Required for any sensible filesystem.
+static int haiga_read(const char *path, char *buf, size_t size, off_t offset,
 		      struct fuse_file_info *fi)
 {
+    printf("READ FUNCTION \n");
 	size_t len;
 	(void) fi;
-	if(strcmp(path, hello_path) != 0)
-		return -ENOENT;
+//	if(strcmp(path, haiga_path) != 0)
+//		return -ENOENT;
 
-	len = strlen(hello_str);
+	len = strlen(haiga_str);
 	if (offset < len) {
 		if (offset + size > len)
 			size = len - offset;
-		memcpy(buf, hello_str + offset, size);
+		memcpy(buf, haiga_str + offset, size);
 	} else
 		size = 0;
 
 	return (int)size;
 }
 
-static struct fuse_operations hello_oper = {
-	.getattr	= hello_getattr,
-	.readdir	= hello_readdir,
-	.open		= hello_open,
-	.read		= hello_read,
+
+
+// Poll for I/O readiness. If ph is non-NULL, when the filesystem is ready for I/O it should call fuse_notify_poll (possibly asynchronously) with the specified ph; this will clear all pending polls. The callee is responsible for destroying ph with fuse_pollhandle_destroy() when ph is no longer needed.
+//static int haiga_poll(const char* path, struct fuse_file_info* fi, struct fuse_pollhandle* ph, unsigned* reventsp)
+//{
+//    return 0;
+//}
+
+
+static struct fuse_operations haiga_operations = {
+	.getattr	= haiga_getattr,
+	.readdir	= haiga_readdir,
+	.open		= haiga_open,
+	.read		= haiga_read,
+    .destroy    = haiga_destroy,
+    .init       = haiga_init,
+    .fgetattr   = haiga_fgetattr,
+//    .access     = haiga_access,
+    .mknod      = haiga_mknod,
+    .write      = haiga_write,
+    .statfs     = haiga_statfs,
+    .release    = haiga_release,
+    .releasedir = haiga_releaseDir,
+    .fsync      = haiga_fsync,
+    .fsyncdir   = haiga_fsyncDir,
+    .flush      = haiga_flush,
+    .lock       = haiga_lock,
+    .bmap       = haiga_bmap,
+    .setxattr   = haiga_setxattr,
+    .getxattr   = haiga_getxattr,
+    .listxattr  = haiga_listxattr,
+//    .ioctl      = haiga_ioctl,
+    .create     = haiga_create,
+    
+    
 };
 
 int main(int argc, char *argv[])
 {
-	return fuse_main(argc, argv, &hello_oper, NULL);
+    for(int i=0 ; i<40 ; i++) {
+        sprintf(fileNamesArr[i], "/%d", i);
+    }
+
+	return fuse_main(argc, argv, &haiga_operations, NULL);
 }
